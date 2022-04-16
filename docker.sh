ba82b292e9188a84c7a37bf6454ec1bd98ffaedf
yuta-0514 <y.akutsu0514@gmail.com>
2022-04-16 19:50:51 +0900

    docker

---
+++ docker.sh
@@ -0,0 +1,12 @@
+~#!/bin/bash
+
+CONTAINER_NAME=detr
+IMAGES=yuta0514/dsfd
+TAGS=1.8
+PORT=8888
+
+docker run --rm -it --gpus all --ipc host -v $PWD:$PWD -v ~/dataset:/mnt -p ${PORT}:${PORT} --name ${CONTAINER_NAME} ${IMAGES}:${TAGS}
+
+#run "umask 000" after this script
+#docker makes file or dir by root, therefore make permission 777
+
