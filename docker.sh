6be1df8c3ec0f0d01ec38cea4d89d74bbd5a66a2
yuta-0514 <y.akutsu0514@gmail.com>
2022-04-28 21:40:47 +0900

    Docker

--- docker.sh
+++ docker.sh
@@ -2,7 +2,7 @@
 
 CONTAINER_NAME=detr
 IMAGES=yuta0514/detr
-TAGS=1.5
+TAGS=1.9
 PORT=8888
 
 docker run --rm -it --gpus all --ipc host -v $PWD:$PWD -v ~/dataset:/mnt -p ${PORT}:${PORT} --name ${CONTAINER_NAME} ${IMAGES}:${TAGS}
