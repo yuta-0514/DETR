8c8d2c0dce45ae19f52cf82723950d526b286981
yuta-0514 <y.akutsu0514@gmail.com>
2022-04-29 19:19:52 +0900

    WIDERFace

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -3,6 +3,7 @@ import torch.utils.data
 import torchvision
 
 from .coco import build as build_coco
+from .widerface import build as build_wider
 
 
 def get_coco_api_from_dataset(dataset):
@@ -17,7 +18,7 @@ def get_coco_api_from_dataset(dataset):
 
 def build_dataset(image_set, args):
     if args.dataset_file == 'coco':
-        return build_coco(image_set, args)
+        return build_wider(image_set, args)
     if args.dataset_file == 'coco_panoptic':
         # to avoid making panopticapi required for coco
         from .coco_panoptic import build as build_coco_panoptic
